1535a3d7e6db52a85a2c13b43b09cfa83c49e8c4
meythabethany <meythabethany@gmail.com>
2023-02-28 14:03:48 +0700

    Environment

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,9 +1,11 @@
-	
+	require("dotenv").config();
+  const{DB_HOST,DB_USER,DB_PASSWORD,DB_NAME} = process.env
+
 const config = {
-    user: "pptsuite",
-    password: "P4nc42011",
-    server: "pptsuite.pancapilartangguh.com",
-    database: "PPTSuite",
+    user: DB_USER,
+    password: DB_PASSWORD,
+    server: DB_HOST,
+    database: DB_NAME,
     options: {
       trustedconnection: true,
       enableArithAbort: true,
