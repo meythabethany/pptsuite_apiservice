ae4690553bdc614e49dc9457359f90a70ba630a9
meythabethany <meythabethany@gmail.com>
2023-03-14 10:18:04 +0700

    SalesOrderSuzuyaController-Meytha

--- controllers/pickingController.js
+++ controllers/pickingController.js
@@ -4,7 +4,7 @@ const sql = require("mssql");
 async function pickingList() {
     try {
       let pool = await sql.connect(config);
-      let res = await pool.request().query("SELECT *  FROM PickHeaderTestApi");
+      let res = await pool.request().query("SELECT  *  FROM PickHeaderTestApi");
       return res.recordsets;
     } catch (error) {
       console.log(" mathus-error :" + error);
---
+++ controllers/salesorderSuzuyaController.js
@@ -0,0 +1,6 @@
+var config = require("../database/dbconfig");
+const sql = require("mssql");
+
+async function createSOsuzuya() {
+
+}
